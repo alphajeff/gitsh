da37a35974878b39a69da2405c15792a7306dbe2
Adam Sharp <adam@sharplet.me>
2019-01-22 08:37:08 -0500

    Change process name to gitsh by embedding Ruby in C
    Fixes #41.
    
    This resolves the process name issue by running a Ruby interpreter
    in-process instead of via `ruby(1)`. The implementation is inspired by
    the article [Running Ruby in C](https://silverhammermba.github.io/emberb/embed/)
    and [Ruby's own `main` function](https://github.com/ruby/ruby/blob/bdbc8a8f1214b601ca409f66f2fe00ac06f2e99a/main.c).
    Instead of invoking `ruby(1)` as a subprocess, we use `ruby.h` and
    `libruby` to perform the same function directly.
    
    In order to test this, I hard-coded the paths to `ruby.h`,
    `ruby/config.h` and `libruby` on my machine. The full set of compiler
    flags, including the platform-specific location of `ruby/config.h`, are
    available via `pkg-config(1)`. I'm not very familiar with Autotools, so
    I could use some help figuring out how to correctly include those flags
    at build time.

--- src/gitsh.c
+++ src/gitsh.c
@@ -1,12 +1,25 @@
 #include <err.h>
+#include <ruby.h>
 #include <sysexits.h>
-#include <unistd.h>
 
 int
 main(int argc, char *argv[])
 {
-    if (execv(GITSH_RB_PATH, argv) == -1) {
-        err(EX_SOFTWARE, GITSH_RB_PATH);
+    int ruby_argc;
+    char *ruby_argv[];
+
+    ruby_argc = argc + 1;
+    ruby_argv = malloc((ruby_argc + 1) * sizeof(char *));
+
+    if (!ruby_argv) {
+        err(EX_OSERR, GITSH_RB_PATH);
     }
-    return EX_OK;
+
+    ruby_argv[0] = argv[0];
+    ruby_argv[1] = GITSH_RB_PATH;
+    memcpy(&ruby_argv[2], &argv[1], argc);
+
+    ruby_init();
+
+    return ruby_run_node(ruby_options(ruby_argc, ruby_argv));
 }
